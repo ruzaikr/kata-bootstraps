6e9c96fac266077e2ebea8f47ba5562610659b14
ruzaikr <ruzaik.rafeek@gmail.com>
2023-12-10 13:25:40 +0100

    Solution for LC 794 (Valid Anagram).

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,24 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        if (s.equals(t)) {
+            return true;
         }
 
-        if (root.left != null || root.right != null) {
-            final TreeNode temp = root.left;
-            root.left = root.right;
-            root.right = temp;
+        final char[] sChars = s.toCharArray();
+
+        final Map<Character, Integer> charOccurrences = new HashMap<>();
+
+        for (char sChar : sChars) {
+            charOccurrences.put(sChar, charOccurrences.getOrDefault(sChar, 0) + 1);
         }
 
-        if (root.left != null) {
-            root.left = invertTree(root.left);
-        }
-        if (root.right != null) {
-            root.right = invertTree(root.right);
+        final char[] tChars = t.toCharArray();
+
+        for (char tChar : tChars) {
+            final int tCharOccurences = charOccurrences.getOrDefault(tChar, 0);
+
+            if (tCharOccurences == 0) {
+                return false;
+            }
+
+            if (tCharOccurences == 1) {
+                charOccurrences.remove(tChar);
+            } else {
+                charOccurrences.put(tChar, tCharOccurences - 1);
+            }
         }
 
-        return root;
+        return charOccurrences.isEmpty();
     }
 
 }
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -1,29 +1,27 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SolutionTest {
 
-    @Test
-    void testInvertBinaryTree() {
-        TreeNode root = new TreeNode(4);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(3);
-        root.right = new TreeNode(7);
-        root.right.left = new TreeNode(6);
-        root.right.right = new TreeNode(9);
+    private static Stream<Arguments> getArgsForTestIsAnagram() {
+        return Stream.of(
+            Arguments.of("a", "a", true),
+            Arguments.of("anagram", "nagaram", true),
+            Arguments.of("anagram", "nagarama", false),
+            Arguments.of("anagram", "nagaxam", false)
+        );
+    }
 
-        Solution solution = new Solution();
-        TreeNode invertedRoot = solution.invertTree(root);
-
-        assertEquals(4, invertedRoot.val);
-        assertEquals(7, invertedRoot.left.val);
-        assertEquals(9, invertedRoot.left.left.val);
-        assertEquals(6, invertedRoot.left.right.val);
-        assertEquals(2, invertedRoot.right.val);
-        assertEquals(3, invertedRoot.right.left.val);
-        assertEquals(1, invertedRoot.right.right.val);
+    @ParameterizedTest
+    @MethodSource("getArgsForTestIsAnagram")
+    void testIsAnagram(String s, String t, boolean expectedIsAnagram) {
+        final Solution solution = new Solution();
+        final boolean isAnagram = solution.isAnagram(s, t);
+        assertEquals(expectedIsAnagram, isAnagram);
     }
 
 }
