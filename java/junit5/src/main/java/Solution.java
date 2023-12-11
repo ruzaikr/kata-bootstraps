76922e4f0e30458a1bad6a0a19c32242422806d9
ruzaikr <ruzaik.rafeek@gmail.com>
2023-12-11 11:15:17 +0100

    LC 235: Lowest Common Ancestor in Binary Search Tree.

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,35 +1,19 @@
 public class Solution {
 
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-        if (image[sr][sc] == color) {
-            return image;
+        TreeNode current = root;
+        while (true) {
+            if (p.val < current.val && q.val < current.val) {
+                current = current.left;
+            } else if (p.val > current.val && q.val > current.val) {
+                current = current.right;
+            } else {
+                break;
+            }
         }
 
-        int prevColor = image[sr][sc];
-        image[sr][sc] = color;
-
-        // top
-        if (sr - 1 >= 0 && image[sr-1][sc] == prevColor) {
-            image =  floodFill(image, sr-1, sc, color);
-        }
-
-        // right
-        if (sc + 1 <= (image[sr].length -1) && image[sr][sc+1] == prevColor) {
-            image =  floodFill(image, sr, sc+1, color);
-        }
-
-        // down
-        if (sr + 1 <= (image.length -1) && image[sr+1][sc] == prevColor) {
-            image =  floodFill(image, sr+1, sc, color);
-        }
-
-        // left
-        if (sc - 1 >= 0 && image[sr][sc-1] == prevColor) {
-            image =  floodFill(image, sr, sc-1, color);
-        }
-
-        return image;
+        return current;
     }
 
 }
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -1,26 +1,53 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
-    private static Stream<Arguments> getArgsForTestSearch() {
-        return Stream.of(
-            Arguments.of(new int[][]{{1,1,1},{1,1,0},{1,0,1}}, 1, 1, 2, new int[][]{{2,2,2},{2,2,0},{2,0,1}}),
-            Arguments.of(new int[][]{{0,0,0},{0,0,0}}, 0, 0, 0, new int[][]{{0,0,0},{0,0,0}})
-        );
+    @Test
+    void testLCA() {
+        final Solution solution = new Solution();
+
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+
+
+        TreeNode p = new TreeNode(7);
+        TreeNode q = new TreeNode(9);
+
+        final TreeNode lca = solution.lowestCommonAncestor(root, p, q);
+
+        assertEquals(8, lca.val);
     }
 
-    @ParameterizedTest
-    @MethodSource("getArgsForTestSearch")
-    void testSearch(int[][] image, int sr, int sc, int color, int[][] expectedColoredImage) {
+    @Test
+    void testLCAWherePIsDescendantOfItself() {
         final Solution solution = new Solution();
-        final int[][] coloredImage = solution.floodFill(image, sr, sc, color);
-        assertTrue(Arrays.deepEquals(expectedColoredImage, coloredImage));
+
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+
+
+        TreeNode p = new TreeNode(2);
+        TreeNode q = new TreeNode(4);
+
+        final TreeNode lca = solution.lowestCommonAncestor(root, p, q);
+
+        assertEquals(2, lca.val);
     }
 
 }
