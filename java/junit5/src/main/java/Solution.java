ab8df9ae957f584e1919c4b96b97375d686cb8a2
ruzaikr <ruzaik.rafeek@gmail.com>
2023-12-11 15:44:59 +0100

    LC 141: Linked List Cycle.

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,19 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        TreeNode current = root;
-        while (true) {
-            if (p.val < current.val && q.val < current.val) {
-                current = current.left;
-            } else if (p.val > current.val && q.val > current.val) {
-                current = current.right;
-            } else {
-                break;
-            }
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
         }
 
-        return current;
+        Set<ListNode> visited = new HashSet<>();
+
+        ListNode current = head;
+        while (current != null) {
+            if (visited.contains(current)) {
+                return true;
+            }
+            visited.add(current);
+            current = current.next;
+        }
+
+        return false;
     }
 
 }
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -1,53 +1,22 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
     @Test
-    void testLCA() {
+    void testIsBalanced() {
         final Solution solution = new Solution();
 
-        TreeNode root = new TreeNode(6);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(0);
-        root.left.right = new TreeNode(4);
-        root.left.right.left = new TreeNode(3);
-        root.left.right.right = new TreeNode(5);
-        root.right = new TreeNode(8);
-        root.right.left = new TreeNode(7);
-        root.right.right = new TreeNode(9);
+        ListNode head = new ListNode(3);
+        ListNode pos = new ListNode(2);
+        head.next = pos;
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = pos;
 
-
-        TreeNode p = new TreeNode(7);
-        TreeNode q = new TreeNode(9);
-
-        final TreeNode lca = solution.lowestCommonAncestor(root, p, q);
-
-        assertEquals(8, lca.val);
+        assertTrue(solution.hasCycle(head));
     }
 
-    @Test
-    void testLCAWherePIsDescendantOfItself() {
-        final Solution solution = new Solution();
-
-        TreeNode root = new TreeNode(6);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(0);
-        root.left.right = new TreeNode(4);
-        root.left.right.left = new TreeNode(3);
-        root.left.right.right = new TreeNode(5);
-        root.right = new TreeNode(8);
-        root.right.left = new TreeNode(7);
-        root.right.right = new TreeNode(9);
-
-
-        TreeNode p = new TreeNode(2);
-        TreeNode q = new TreeNode(4);
-
-        final TreeNode lca = solution.lowestCommonAncestor(root, p, q);
-
-        assertEquals(2, lca.val);
-    }
 
 }
