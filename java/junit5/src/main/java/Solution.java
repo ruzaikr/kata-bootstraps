46efbd9a22f7d1e45f3b19aa7d6dea3b359f35e3
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-02-13 17:53:30 +0100

    K Closest Points to Origin Solution.

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,62 +1,30 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Solution {
 
+    public int[][] kClosest(int[][] points, int k) {
 
-    public int[][] updateMatrix(int[][] mat) {
+        final PriorityQueue<int[]> minHeap = new PriorityQueue<>(points.length, Comparator.comparingInt(ints -> ints[0]));
 
-        final Queue<int[]> queue = new LinkedList<>();
-
-        final boolean[][] visited = new boolean[mat.length][mat[0].length];
-        final int[][] distances = new int[mat.length][mat[0].length];
-
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                if (mat[i][j] == 0) {
-                    visited[i][j] = true;
-                    // no need to update `distances` because all values in `distances` are 0 after initialization
-                    queue.add(new int[]{i,j});
-                }
-            }
+        for (int[] point : points) {
+            final int distance = getDistance(point[0], point[1]);
+            minHeap.add(new int[]{distance, point[0], point[1]});
         }
 
-        while (!queue.isEmpty()) {
-            final int[] currentIJ = queue.remove();
-            final int currentI = currentIJ[0];
-            final int currentJ = currentIJ[1];
+        final int[][] kClosestPoints = new int[k][2];
 
-            final int distanceIJ = distances[currentI][currentJ];
-
-            final int[][] addendsToIJ = new int[][]{
-                    {0, 1},
-                    {1, 0},
-                    {0, -1},
-                    {-1, 0}
-            };
-
-            for (int[] addendToIJ : addendsToIJ) {
-                final int newI = currentI + addendToIJ[0];
-                final int newJ = currentJ + addendToIJ[1];
-
-                if (isIJWithinBounds(newI, newJ, mat) && !visited[newI][newJ]) {
-                    visited[newI][newJ] = true;
-                    distances[newI][newJ] = distanceIJ + 1;
-                    queue.add(new int[]{newI, newJ});
-                }
-            }
-
-
+        for (int i = 0; i < k; i++) {
+            final int[] point = minHeap.remove();
+            kClosestPoints[i][0] = point[1];
+            kClosestPoints[i][1] = point[2];
         }
 
-        return distances;
-
+        return kClosestPoints;
     }
 
-    private boolean isIJWithinBounds(final int i, final int j, final int[][] mat) {
-        return i >= 0 && i < mat.length && j >= 0 && j < mat[i].length;
+    private int getDistance(final int x1, final int y1) {
+        return (int) (Math.pow(Math.abs(x1), 2) + Math.pow(Math.abs(y1), 2));
     }
-
-
 
 }
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,29 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class SolutionTest {
 
-    private static Stream<Arguments> getArgsForTestUpdateMatrix() {
+    private static Stream<Arguments> getArgsForTestKClosest() {
         return Stream.of(
-                Arguments.of(new int[][]{{0,0,0},{0,1,0},{0,0,0}}, new int[][]{{0,0,0},{0,1,0},{0,0,0}}),
-                Arguments.of(new int[][]{{0,0,0},{0,1,0},{1,1,1}}, new int[][]{{0,0,0},{0,1,0},{1,2,1}})
+                Arguments.of(new int[][]{{1,3},{-2,2}}, 1, new int[][]{{-2,2}}),
+                Arguments.of(new int[][]{{3,3},{5,-1},{-2,4}}, 2, new int[][]{{3,3},{-2,4}})
         );
     }
 
 
     @ParameterizedTest
-    @MethodSource("getArgsForTestUpdateMatrix")
-    void testUpdateMatrix(final int[][] mat, final int[][] expectedUpdatedMat) {
+    @MethodSource("getArgsForTestKClosest")
+    void testKClosest(final int[][] points, final int k, final int[][] ExpectedKClosestPoints) {
         final Solution solution = new Solution();
 
-        final int[][] updatedMat = solution.updateMatrix(mat);
+        final int[][] kClosestPoints = solution.kClosest(points, k);
 
-        assertArrayEquals(expectedUpdatedMat, updatedMat);
-    }
-
-    @Test
-    void testPlayground() {
-        boolean[][] myMatrix = new boolean[3][3];
-
-        System.out.println(myMatrix[1][1]);
+        assertArrayEquals(ExpectedKClosestPoints, kClosestPoints);
     }
 
 }
