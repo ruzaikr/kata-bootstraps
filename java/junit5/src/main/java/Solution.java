c97f3d4500abb3fb4fbd9189683eb6522c8af4a9
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-02-14 23:58:18 +0100

    Clone Graph Solution.

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,61 +1,26 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 
-    public List<List<Integer>> threeSum(int[] nums) {
+    private final Map<Node, Node> origToClone = new HashMap<>();
 
-        final List<Integer> sortedNums = Arrays.stream(nums).sorted().boxed().toList();
+    public Node cloneGraph(Node node) {
 
-        final List<List<Integer>> triplets = new LinkedList<>();
-
-        final Set<Integer> processedFirstDigits = new HashSet<>();
-
-        for (int i = 0; i < (sortedNums.size() - 2); i++) {
-            if (!processedFirstDigits.contains(sortedNums.get(i))) {
-                processedFirstDigits.add(sortedNums.get(i));
-
-                final Set<Integer> processedSecondDigits = new HashSet<>();
-                final Set<Integer> processedThirdDigits = new HashSet<>();
-
-                int leftPtr = i + 1;
-                int rightPtr = sortedNums.size() - 1;
-
-                while (leftPtr < rightPtr) {
-
-                    if (processedSecondDigits.contains(sortedNums.get(leftPtr))) {
-                        leftPtr++;
-                         continue;
-                    }
-
-                    if (processedThirdDigits.contains(sortedNums.get(rightPtr))) {
-                        rightPtr--;
-                         continue;
-                    }
-
-                    int sum = sortedNums.get(i) + sortedNums.get(leftPtr);
-                    if (sum > 0) {
-                        break;
-                    }
-                    sum += sortedNums.get(rightPtr);
-
-                    if (sum > 0) {
-                        processedThirdDigits.add(sortedNums.get(rightPtr));
-                        rightPtr--;
-                    } else if (sum < 0) {
-                        processedSecondDigits.add(sortedNums.get(leftPtr));
-                        leftPtr++;
-                    } else {
-                        triplets.add(List.of(sortedNums.get(i), sortedNums.get(leftPtr), sortedNums.get(rightPtr)));
-                        processedSecondDigits.add(sortedNums.get(leftPtr));
-                        leftPtr++;
-                    }
-
-                }
-
-            }
+        if (node == null || node.neighbors == null) {
+            return node;
         }
 
-        return triplets;
+        if (origToClone.containsKey(node)) {
+            return origToClone.get(node);
+        }
+
+        final Node clone = new Node(node.val);
+        origToClone.put(node, clone);
+        for (final Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+        return clone;
 
     }
 
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -1,7 +1,10 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,23 +12,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
 
-    private static Stream<Arguments> getArgsForTestKClosest() {
-        return Stream.of(
-//                Arguments.of(new int[]{1,-1,-1,0}, List.of(List.of(-1,0,1))),
-//                Arguments.of(new int[]{0,0,0}, List.of(List.of(0,0,0))),
-                Arguments.of(new int[]{-1,0,1,2,-1,-4}, List.of(List.of(-1,-1,2),List.of(-1,0,1)))
-        );
-    }
-
-
-    @ParameterizedTest
-    @MethodSource("getArgsForTestKClosest")
-    void testKClosest(final int[] nums, final List<List<Integer>> expectedTriplets) {
+    @Test
+    void testCloneGraph() {
         final Solution solution = new Solution();
 
-        final List<List<Integer>> triplets = solution.threeSum(nums);
+        final Node node1 = new Node(1);
+        final Node node2 = new Node(2);
+        final Node node3 = new Node(3);
+        final Node node4 = new Node(4);
 
-        assertEquals(expectedTriplets, triplets);
+        node1.neighbors.add(node2);
+        node1.neighbors.add(node4);
+
+        node2.neighbors.add(node1);
+        node2.neighbors.add(node3);
+
+        node3.neighbors.add(node2);
+        node3.neighbors.add(node4);
+
+        node4.neighbors.add(node1);
+        node4.neighbors.add(node3);
+
+        final Node outputNode = solution.cloneGraph(node1);
+        System.out.println(outputNode);
     }
 
 }
