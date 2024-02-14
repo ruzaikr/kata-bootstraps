c94c7ab6011e4477ec6971e1202d31d8019e564f
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-02-14 21:34:10 +0100

    Three Sum Solution.

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,39 +1,61 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
 
-    public int lengthOfLongestSubstring(String s) {
+    public List<List<Integer>> threeSum(int[] nums) {
 
-        if (s.isEmpty()) {
-            return 0;
+        final List<Integer> sortedNums = Arrays.stream(nums).sorted().boxed().toList();
+
+        final List<List<Integer>> triplets = new LinkedList<>();
+
+        final Set<Integer> processedFirstDigits = new HashSet<>();
+
+        for (int i = 0; i < (sortedNums.size() - 2); i++) {
+            if (!processedFirstDigits.contains(sortedNums.get(i))) {
+                processedFirstDigits.add(sortedNums.get(i));
+
+                final Set<Integer> processedSecondDigits = new HashSet<>();
+                final Set<Integer> processedThirdDigits = new HashSet<>();
+
+                int leftPtr = i + 1;
+                int rightPtr = sortedNums.size() - 1;
+
+                while (leftPtr < rightPtr) {
+
+                    if (processedSecondDigits.contains(sortedNums.get(leftPtr))) {
+                        leftPtr++;
+                         continue;
+                    }
+
+                    if (processedThirdDigits.contains(sortedNums.get(rightPtr))) {
+                        rightPtr--;
+                         continue;
+                    }
+
+                    int sum = sortedNums.get(i) + sortedNums.get(leftPtr);
+                    if (sum > 0) {
+                        break;
+                    }
+                    sum += sortedNums.get(rightPtr);
+
+                    if (sum > 0) {
+                        processedThirdDigits.add(sortedNums.get(rightPtr));
+                        rightPtr--;
+                    } else if (sum < 0) {
+                        processedSecondDigits.add(sortedNums.get(leftPtr));
+                        leftPtr++;
+                    } else {
+                        triplets.add(List.of(sortedNums.get(i), sortedNums.get(leftPtr), sortedNums.get(rightPtr)));
+                        processedSecondDigits.add(sortedNums.get(leftPtr));
+                        leftPtr++;
+                    }
+
+                }
+
+            }
         }
 
-        int longestLength = 0;
-
-        final Set<Character> subString = new HashSet<>();
-
-        final char[] charsOfS = s.toCharArray();
-
-        int leftPtr = 0;
-
-        for (int rightPtr = 0; rightPtr < s.length(); rightPtr++) {
-
-            while (subString.contains(charsOfS[rightPtr])) {
-                subString.remove(charsOfS[leftPtr]);
-                leftPtr++;
-            }
-
-            subString.add(charsOfS[rightPtr]);
-
-            if (subString.size() > longestLength) {
-                longestLength = subString.size();
-            }
-        }
-
-        return longestLength;
+        return triplets;
 
     }
 
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,23 +11,21 @@ public class SolutionTest {
 
     private static Stream<Arguments> getArgsForTestKClosest() {
         return Stream.of(
-                Arguments.of("abcabcbb", 3),
-                Arguments.of("bbbbb", 1),
-                Arguments.of("pwwkew", 3),
-                Arguments.of("dvdf", 3),
-                Arguments.of("abba", 2)
+//                Arguments.of(new int[]{1,-1,-1,0}, List.of(List.of(-1,0,1))),
+//                Arguments.of(new int[]{0,0,0}, List.of(List.of(0,0,0))),
+                Arguments.of(new int[]{-1,0,1,2,-1,-4}, List.of(List.of(-1,-1,2),List.of(-1,0,1)))
         );
     }
 
 
     @ParameterizedTest
     @MethodSource("getArgsForTestKClosest")
-    void testKClosest(final String s, final int expectedOutput) {
+    void testKClosest(final int[] nums, final List<List<Integer>> expectedTriplets) {
         final Solution solution = new Solution();
 
-        final int output = solution.lengthOfLongestSubstring(s);
+        final List<List<Integer>> triplets = solution.threeSum(nums);
 
-        assertEquals(expectedOutput, output);
+        assertEquals(expectedTriplets, triplets);
     }
 
 }
