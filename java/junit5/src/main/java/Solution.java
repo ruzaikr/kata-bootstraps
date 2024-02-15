1fbfa2ace937a2d172f71d3940553c4e4b4e0d02
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-02-15 22:50:56 +0100

    Coin Change Solution.

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,8 +1,45 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 
+    public int coinChange(int[] coins, int amount) {
 
+        final Map<Integer, Integer> dp = new HashMap<>();
+        dp.put(0, 0);
 
+        for (int i = 1; i <= amount; i++) {
+            int minNumCoinsNeeded = amount + 1;
+            for (int coinDenomination : coins) {
+
+                if (coinDenomination > i) {
+                    continue;
+                }
+
+                if (coinDenomination == i) {
+                    minNumCoinsNeeded = 1;
+                    continue;
+                }
+
+                final int remainder = i - coinDenomination;
+                if (!dp.containsKey(remainder)) {
+                    throw new RuntimeException("boom!");
+                }
+
+                final int numCoinsNeeded = 1 + dp.get(remainder);
+                minNumCoinsNeeded = Math.min(numCoinsNeeded, minNumCoinsNeeded);
+            }
+            dp.put(i, minNumCoinsNeeded);
+        }
+
+        final int output = dp.get(amount);
+
+        if (output == amount + 1) {
+            return -1;
+        }
+
+        return output;
+
+    }
 
 }
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -1,16 +1,31 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
 
-    @Test
-    void testTrie() {
-        Trie trie = new Trie();
-        trie.insert("apple");
+    private static Stream<Arguments> getArgsForTestCoinChange() {
+        return Stream.of(
+                Arguments.of(new int[]{2}, 4, 2),
+                Arguments.of(new int[]{2}, 3, -1),
+                Arguments.of(new int[]{1,2,5}, 11, 3),
+                Arguments.of(new int[]{1,2,5}, 0, 0)
+        );
+    }
 
-        assertTrue(trie.search("apple"));
-        assertTrue(trie.startsWith("ap"));
+    @ParameterizedTest
+    @MethodSource("getArgsForTestCoinChange")
+    void testCoinChange(final int[] coins, final int amount, final int expectedOutput) {
+
+        final Solution solution = new Solution();
+
+        final int output = solution.coinChange(coins, amount);
+
+        assertEquals(expectedOutput, output);
 
     }
 
