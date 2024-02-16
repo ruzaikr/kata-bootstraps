c3307a750c12ba1af0cd49f244ee52489d3e59f0
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-02-16 21:42:22 +0100

    Is Valid BST Solution.

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,5 +1,26 @@
 public class Solution {
 
+    public boolean isValidBST(TreeNode root) {
 
+
+        return dfs(root, null, null);
+
+    }
+
+    private boolean dfs(TreeNode root, Integer lowerBound, Integer upperBound) {
+        if (root == null) {
+            return true;
+        }
+
+        if (lowerBound != null && root.val <= lowerBound) {
+            return false;
+        }
+
+        if (upperBound != null && root.val >= upperBound) {
+            return false;
+        }
+
+        return dfs(root.left, lowerBound, root.val) && dfs(root.right, root.val, upperBound);
+    }
 
 }
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -1,4 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SolutionTest {
+
+    @Test
+    void testIsValidBST() {
+        final TreeNode root = new TreeNode(2147483647);
+
+        final Solution solution = new Solution();
+
+        final boolean output = solution.isValidBST(root);
+
+        assertTrue(output);
+
+    }
 
 
 
