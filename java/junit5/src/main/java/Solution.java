376b5316cc101f5351bc00f3f474543e87dc3390
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-02-22 13:57:25 +0100

    Initial commit

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,20 +1,4 @@
 public class Solution {
 
-    public TreeNode invertTree(TreeNode root) {
-
-        if (root == null) {
-            return null;
-        }
-
-        TreeNode invertedRightTree = invertTree(root.right);
-        TreeNode invertedLeftTree = invertTree(root.left);
-
-        root.left = invertedRightTree;
-        root.right = invertedLeftTree;
-
-        return root;
-
-    }
-
 
 }
