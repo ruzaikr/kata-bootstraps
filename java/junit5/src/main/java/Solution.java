32c6c12c19f134a96c6e6b026664e1e73180c9bd
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-02-24 00:14:00 +0100

    Longest Ski Path Solution

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,88 +1,47 @@
-public class Solution {
+class Solution {
 
-    public int maximalRectangle(char[][] matrix) {
+    public int getLongestPath(int[][] matrix) {
 
-        int[][] prefixSums = getPrefixSums(matrix);
-
-        int maxSum = 0;
-
-        for (int row1 = 0; row1 < matrix.length; row1++) {
-            for (int col1 = 0; col1 < matrix[0].length; col1++) {
-                for (int row2 = 0; row2 < matrix.length; row2++) {
-                    for (int col2 = 0; col2 < matrix[0].length; col2++) {
-
-                       int sum = getSum(prefixSums, row1, col1, row2, col2);
-                       int area = getArea(row1, col1, row2, col2);
-                       if (sum == area && sum > maxSum) {
-                           maxSum = sum;
-                       }
-
-                    }
-
-                }
-
-            }
-
-        }
-
-        return maxSum;
-
-    }
-
-    public int getArea(int row1, int col1, int row2, int col2) {
-        int length = Math.abs(row1-row2) + 1;
-        int width = Math.abs(col1-col2) + 1;
-        return length*width;
-    }
-
-    public int getSum(int[][] prefixSums, int row1, int col1, int row2, int col2) {
-        int bottomRight = prefixSums[row2][col2];
-        int top = 0;
-        int left = 0;
-        int topLeft = 0;
-
-        if (row1 > 0) {
-            top = prefixSums[row1-1][col2];
-        }
-
-        if (col1 > 0) {
-            left = prefixSums[row2][col1-1];
-        }
-
-        if (row1 > 0 && col1 > 0) {
-            topLeft = prefixSums[row1-1][col1-1];
-        }
-
-        return bottomRight - top - left + topLeft;
-    }
-
-    public int[][] getPrefixSums(char[][] matrix) {
-        int[][] prefixSums = new int[matrix.length][matrix[0].length];
-
+        int longestPathSize = 0;
         for (int row = 0; row < matrix.length; row++) {
-
-            int[] prefixSumsRow = new int[matrix[0].length];
-            prefixSumsRow[0] = matrix[row][0] - '0';
             for (int col = 0; col < matrix[0].length; col++) {
-                int currentVal = matrix[row][col] - '0';
-                int up = 0;
-                int left = 0;
-
-                if (row > 0) {
-                    up = prefixSums[row-1][col];
+                int pathSize = dfs(matrix, 1, row, col);
+                if (pathSize > longestPathSize) {
+                    longestPathSize = pathSize;
                 }
-
-                if (col > 0) {
-                    left = prefixSumsRow[col-1];
-                    prefixSumsRow[col] = left + currentVal;
-                }
-
-                prefixSums[row][col] = currentVal + up + left;
             }
-
         }
 
-        return prefixSums;
+        return longestPathSize;
+
+    }
+
+    private int dfs(int[][] matrix, int currentPathSize, int row, int col) {
+        int height = matrix[row][col];
+        int longestPathSize = currentPathSize;
+
+        int[][] deltas = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+        for (int[] delta : deltas) {
+            int nextRow = row + delta[0];
+            int nextCol = col + delta[1];
+
+            if (!isValid(nextRow, nextCol, height, matrix)) {
+                continue;
+            }
+
+            int pathSizeAfterTraversal = dfs(matrix, currentPathSize + 1, nextRow, nextCol);
+
+            if (pathSizeAfterTraversal > longestPathSize) {
+                longestPathSize = pathSizeAfterTraversal;
+            }
+        }
+
+        return longestPathSize;
+
+    }
+
+    private boolean isValid(int row, int col, int prevHeight, int[][] matrix) {
+        return row >= 0 && col >= 0 && row < matrix.length && col < matrix[0].length && matrix[row][col] < prevHeight;
     }
 
 }
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -1,31 +1,28 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
     private static Stream<Arguments> getArgs() {
         return Stream.of(
-                Arguments.of(new char[][]{
-                        {'1','0','1','0','0'},
-                        {'1','0','1','1','1'},
-                        {'1','1','1','1','1'},
-                        {'1','0','0','1','0'}
-                }, 6)
+                Arguments.of(new int[][]{
+                        {6,1,2},
+                        {1,4,3}
+                }, 4)
         );
     }
 
     @ParameterizedTest
     @MethodSource("getArgs")
-    void testMaximalRectangle(char[][] matrix, int expectedOutput) {
+    void testMaximalRectangle(int[][] matrix, int expectedLongestPathSize) {
         Solution solution = new Solution();
-        int output = solution.maximalRectangle(matrix);
-        assertEquals(expectedOutput, output);
+        int longestPathSize = solution.getLongestPath(matrix);
+        assertEquals(expectedLongestPathSize, longestPathSize);
     }
 
 }
