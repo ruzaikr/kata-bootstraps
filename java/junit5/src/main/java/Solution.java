bce6e336d8b4443e40761e61a9f8e308f7575b7b
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-02-25 01:42:47 +0100

    0 1 Matrix 2nd attempt

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -3,63 +3,58 @@ import java.util.*;
 class Solution {
 
     /*
+    *
+    * 1. Iterate over all cells in the matrix and enqueue all cells containing 0
+    * 2. While queue is not empty, dequeue a cell and process its neighbors
+    *
+    * */
 
-    1. Starting from the root, traverse the entire tree using dfs.
-    2. During the dfs, insert the treeNode into a hashMap where the key is the x-axis value of the treeNode.
-    3. To get the x-axis value of the treeNode, we need to pass an x-axis value from the caller of dfs.
+    public int[][] updateMatrix(int[][] mat) {
 
-    4. Iterate over the keys of the hashMap
+        int[][] distances = new int[mat.length][mat[0].length];
+        boolean[][] visited = new boolean[mat.length][mat[0].length];
 
+        Queue<int[]> queue = new LinkedList<>();
 
-     */
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[0].length; col++) {
+                if (mat[row][col] == 0) {
+                    queue.add(new int[]{row,col,0});
+                }
+            }
+        }
 
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        while (!queue.isEmpty()) {
+            int[] rowColDist = queue.remove();
+            int row = rowColDist[0];
+            int col = rowColDist[1];
+            int distance = rowColDist[2];
 
-        Map<Integer, Map<Integer, List<Integer>>> xToYToVals = new HashMap<>();
+            if (visited[row][col]) {
+                continue;
+            }
 
-        dfs(root, 0, 0, xToYToVals);
+            visited[row][col] = true;
+            distances[row][col] = distance;
 
-        List<Integer> sortedKeySetX = xToYToVals.keySet().stream().sorted().toList();
-        List<List<Integer>> result = new LinkedList<>();
-
-        for (int x : sortedKeySetX) {
-
-            Map<Integer, List<Integer>> yToVals = xToYToVals.get(x);
-
-            List<Integer> sortedKeySetY = yToVals.keySet().stream().sorted().toList();
-
-            List<Integer> nameItLater = new LinkedList<>();
-
-            for (int y : sortedKeySetY) {
-                List<Integer> vals = yToVals.get(y);
-                if (vals.size() == 1) {
-                    nameItLater.add(vals.get(0));
-                } else {
-                    nameItLater.addAll(vals.stream().sorted().toList());
+            int[][] deltas = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+            for (int[] delta : deltas) {
+                int newRow = row+delta[0];
+                int newCol = col+delta[1];
+                if (isValid(newRow, newCol, mat) && !visited[newRow][newCol]) {
+                    queue.add(new int[]{newRow,newCol,distance+1});
                 }
             }
 
-            result.add(nameItLater);
-
         }
 
-        return result;
+        return distances;
 
     }
 
-    private void dfs(TreeNode treeNode, int x, int y, Map<Integer, Map<Integer, List<Integer>>> xToYToVals) {
-        if (treeNode == null) {
-            return;
-        }
-
-        Map<Integer, List<Integer>> yToVals = xToYToVals.getOrDefault(x, new HashMap<>());
-        List<Integer> vals = yToVals.getOrDefault(y, new LinkedList<>());
-        vals.add(treeNode.val);
-        yToVals.put(y, vals);
-        xToYToVals.put(x, yToVals);
-
-        dfs(treeNode.left, x - 1, y + 1, xToYToVals);
-        dfs(treeNode.right, x + 1, y + 1, xToYToVals);
+    private boolean isValid(int row, int col, int[][] matrix) {
+        return row >= 0 && col >= 0 && row < matrix.length && col < matrix[0].length;
     }
+
 
 }
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -7,24 +7,28 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
     @Test
-    void testVerticalTraversal() {
-        TreeNode root = new TreeNode(1,
-                new TreeNode(2,
-                        new TreeNode(4),
-                        new TreeNode(5)),
-                new TreeNode(3,
-                        new TreeNode(6),
-                        new TreeNode(7))
-        );
+    void testMatrix() {
+        int[][] matrix = new int[][]{
+                {0,0,0},
+                {0,1,0},
+                {1,1,1}
+        };
 
-        List<List<Integer>> output = new Solution().verticalTraversal(root);
+        int[][] output = new Solution().updateMatrix(matrix);
 
-        assertEquals(List.of(List.of(4), List.of(2), List.of(1,5,6), List.of(3), List.of(7)), output);
+        int[][] expectedOutput = new int[][]{
+                {0,0,0},
+                {0,1,0},
+                {1,2,1}
+        };
+
+        assertArrayEquals(expectedOutput, output);
     }
 
 }
