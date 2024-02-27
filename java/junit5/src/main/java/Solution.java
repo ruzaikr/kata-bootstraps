8c1363ed24ae329abb8740aaf8c2d7b6760e4274
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-02-27 17:22:12 +0100

    Clone Graph Second Attempt

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.Stream;
 
 class Solution {
 
@@ -8,32 +7,41 @@ class Solution {
     *
     * */
 
-    protected void backtrack(int remain, LinkedList<Integer> comb, int start, int[] candidates, List<List<Integer>> results) {
 
-        if (remain == 0) {
-            // make a deep copy of the current combination
-            results.add(new ArrayList<Integer>(comb));
-            return;
-        } else if (remain < 0) {
-            // exceed the scope, stop exploration.
-            return;
+    public Node cloneGraph(Node node) {
+
+        if (node == null) {
+            return null;
         }
 
-        for (int i = start; i < candidates.length; ++i) {
-            // add the number into the combination
-            comb.add(candidates[i]);
-            this.backtrack(remain - candidates[i], comb, i, candidates, results);
-            // backtrack, remove the number from the combination
-            comb.removeLast();
+        Map<Integer, Node> valToNodeCopy = new HashMap<>();
+        Set<Node> visited = new HashSet<>();
+
+        Node nodeCopy = null;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            Node currNode = queue.remove();
+            visited.add(currNode);
+            Node currNodeCopy = valToNodeCopy.getOrDefault(currNode.val, new Node(currNode.val));
+            if (nodeCopy == null) {
+                nodeCopy = currNodeCopy;
+            }
+            List<Node> neighborsCopy = new LinkedList<>();
+            for (Node neighbor : currNode.neighbors) {
+                Node neighborCopy = valToNodeCopy.getOrDefault(neighbor.val, new Node(neighbor.val));
+                valToNodeCopy.put(neighbor.val, neighborCopy);
+                neighborsCopy.add(neighborCopy);
+                if (!visited.contains(neighbor)) {
+                    queue.add(neighbor);
+                }
+            }
+            currNodeCopy.neighbors = neighborsCopy;
+            valToNodeCopy.put(currNode.val, currNodeCopy);
         }
-    }
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<List<Integer>>();
-        LinkedList<Integer> comb = new LinkedList<Integer>();
-
-        this.backtrack(target, comb, 0, candidates, results);
-        return results;
+        return nodeCopy;
     }
 
 
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -4,23 +4,31 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
     @Test
-    void testCombinationSum() {
+    void testLevelOrder() {
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node = new Node(1);
+        Node node4 = new Node(4);
+
+        node.neighbors = List.of(node2, node4);
+        node2.neighbors = List.of(node, node3);
+        node3.neighbors = List.of(node2, node4);
+        node4.neighbors = List.of(node, node3);
+
         Solution solution = new Solution();
-        List<List<Integer>> output = solution.combinationSum(new int[]{2,3,6,7}, 7);
-        List<List<Integer>> expected = List.of(
-                List.of(2,2,3),
-                List.of(7)
-        );
-        assertArrayEquals(expected.toArray(), output.toArray());
+        Node nodeCopy = solution.cloneGraph(node);
+        assertNotEquals(node, nodeCopy);
+
+
     }
 
 
