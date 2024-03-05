2f539bcaa3ca59fae5ba005c8165ddf0462cb361
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-03-05 15:04:15 +0100

    Course Schdule 2nd Attempt Solution

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,155 +1,58 @@
-import java.util.*;
-
-/*
-
-Session Log Analysis:
-
-An operation can be a "start session", or an "end session", or another type of
-operation. Each line of the log contains the timestamp, the session ID and the
-operation, in order.
-<timestamp>,<session_id>,<operation>
-
-Definitions:
-
-- Complete session: A session that starts and ends is a complete session. The
-length of a session is for how long it was open.
-
-- Open session: A session that starts but doesn't end is an open session.
-
-- Out of session operation: An operation that cannot be associated with a
-previous "start session", is an out of session operation. An "end session" can
-also be out of session.
-
-Tasks:
-
-1. Count number of open sessions, complete sessions, and out of session
-operations.
-
-2. Given number N, return the IDs of the longest completed N sessions, in order.
-
-3. Find the maximum number of simultaneously open sessions.
-
-Note: The main input is a multi-line string containing the log. The input is
-always valid, meaning the same session cannot be started twice, timestamps are
-in chronological order, and there are no blank lines.
-
-Example Log:
-
-In the log below, there are three sessions, 5, 7, and 10. In session 7, we have
-4 operations including 2 operations to start and end the session. Session 7 is
-a *complete session*, the operation at time 1221 is an
-*out of session operation* and session 10 is an *open session* at the end of the
-log. Also, session 7 was *open* for 5 seconds from 1234 to 1239. So, the length
-of a session is its end time minus its start time.
-
-1221,5,read a file
-1234,7,start session
-1235,7,read a file
-1236,7,write a record
-1239,10,start session
-1239,7,end session
-1242,10,edit a record
-
- */
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
 
-    /*
-    *
-    *
-    * */
+        Map<Integer, Set<Integer>> courseNumToPrereqs = new HashMap<>();
 
-    private static List<Integer> longestCompletedSessions(List<List<String>> triplets, int n) {
+        int largestCourseNum = numCourses - 1;
 
-        Map<Integer, Integer> sessionIdToOpenSessionTime = new HashMap<>();
-
-        PriorityQueue<int[]> completedSessions = new PriorityQueue<>(Comparator.comparingInt(ints -> ints[1]));
-
-        for (List<String> triplet : triplets) {
-            int sessionId = getSessionId(triplet);
-            if (isOpenOperation(triplet)) {
-                sessionIdToOpenSessionTime.put(sessionId, getTimeStamp(triplet));
-            }
-
-            if (isEndOperation(triplet) && sessionIdToOpenSessionTime.containsKey(sessionId)) {
-                int sessionLength = sessionIdToOpenSessionTime.get(sessionId) - getTimeStamp(triplet);
-                completedSessions.add(new int[]{sessionLength, sessionId});
-            }
-        }
-
-        List<Integer> result = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            result.add(completedSessions.remove()[1]);
-        }
-
-        return result;
-    }
-
-    public static void main(String[] args) {
-
-        String input = "1221,5,read a file\n" +
-                "1234,7,start session\n" +
-                "1235,7,read a file\n" +
-                "1236,7,write a record\n" +
-                "1239,10,start session\n" +
-                "1239,7,end session\n" +
-                "1242,10,edit a record";
-
-        List<String> operations = List.of(input.split("\n"));
-
-        List<List<String>> listOfOperationTriplets = operations.stream().map(operation -> List.of(operation.split(","))).toList();
-
-        Set<Integer> openSessionIds = new HashSet<>();
-
-        int outOfSessionOperations = 0;
-        int completeSessions = 0;
-        for (List<String> triplet : listOfOperationTriplets) {
-            int sessionId = getSessionId(triplet);
-            if (isOpenOperation(triplet)) {
-                openSessionIds.add(sessionId);
+        for (int[] prereq : prerequisites) {
+            int courseNum = prereq[0];
+            int prereqNum = prereq[1];
+            if (courseNum > largestCourseNum || prereqNum > largestCourseNum) {
                 continue;
             }
-            if (isEndOperation(triplet)) {
-                if (openSessionIds.contains(sessionId)) {
-                    openSessionIds.remove(sessionId);
-                    completeSessions++;
-                }
-            }
-            if(isNotOpenNorEndOperation(triplet) && !openSessionIds.contains(sessionId)) {
-                outOfSessionOperations++;
-            }
-
+            Set<Integer> prereqs = courseNumToPrereqs.getOrDefault(courseNum, new HashSet<>());
+            prereqs.add(prereqNum);
+            courseNumToPrereqs.put(courseNum, prereqs);
         }
-        int openSession = openSessionIds.size();
 
-        System.out.println(openSession);
-        System.out.println(completeSessions);
-        System.out.println(outOfSessionOperations);
 
-        System.out.println(longestCompletedSessions(listOfOperationTriplets, 2));
+        for (int courseNum : courseNumToPrereqs.keySet()) {
+
+            Set<Integer> path = new HashSet<>();
+            path.add(courseNum);
+
+            if (!dfs(courseNum, path, courseNumToPrereqs)) {
+                return false;
+            }
+        }
+
+        return true;
 
     }
 
-    private static Integer getSessionId(List<String> triplet) {
-        return Integer.parseInt(triplet.get(1));
+    private boolean dfs(int courseNum, Set<Integer> path, Map<Integer, Set<Integer>> courseNumToPrereqs) {
+
+        if (!courseNumToPrereqs.containsKey(courseNum)) {
+            return true;
+        }
+
+        for (int prereqNum : courseNumToPrereqs.get(courseNum)) {
+            if (path.contains(prereqNum)) {
+                return false;
+            }
+            Set<Integer> newPath = new HashSet<>(path);
+            newPath.add(prereqNum);
+            if (!dfs(prereqNum, newPath, courseNumToPrereqs)) {
+                return false;
+            }
+        }
+
+        return true;
     }
-
-    private static Integer getTimeStamp(List<String> triplet) {
-        return Integer.parseInt(triplet.get(0));
-    }
-
-    private static boolean isOpenOperation(List<String> triplet) {
-        return triplet.get(2).equals("start session");
-    }
-
-    private static boolean isEndOperation(List<String> triplet) {
-        return triplet.get(2).equals("end session");
-    }
-
-    private static boolean isNotOpenNorEndOperation(List<String> triplet) {
-        return !isEndOperation(triplet) && !isOpenOperation(triplet);
-    }
-
-
-
 }
--- java/junit5/src/test/java/SolutionTest.java
+++ java/junit5/src/test/java/SolutionTest.java
@@ -12,10 +12,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
+    private static Stream<Arguments> getArgs() {
+        return Stream.of(
+                Arguments.of(7, new int[][]{{1,2}, {3,4}, {4,6}, {5,1}, {5,3}, {6,5}}, false),
+                Arguments.of(5, new int[][]{{1,3}, {1,2}, {3,4}, {2,4},}, true),
+                Arguments.of(5, new int[][]{{2,4}, {3,1}, {3,2}, {1,4},}, true),
+                Arguments.of(2, new int[][]{{1,0}}, true)
+        );
+    }
 
-    @Test
-    void testCombinationSum() {
-
+    @ParameterizedTest
+    @MethodSource("getArgs")
+    void testCanFinish(int numCourses, int[][] prerequisites, boolean expectedOutput) {
+        Solution solution = new Solution();
+        assertEquals(expectedOutput, solution.canFinish(numCourses, prerequisites));
     }
 
 }
