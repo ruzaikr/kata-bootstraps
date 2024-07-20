8f596c5fdca73b43cd7d230b7a6c6dcefd7460ad
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-07-20 13:50:50 +0530

    Solution for Valid Parantheses

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,19 +1,48 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class Solution {
 
-    public int[] getIndices(int target, int[] nums) {
-        Map<Integer, Integer> numberToIndex = new HashMap<>();
+    public boolean isValid(String s) {
 
-        for (int i = 0; i < nums.length; i++) {
-            int diff = target - nums[i];
-            if (numberToIndex.containsKey(diff)) {
-                return new int[]{i, numberToIndex.get(diff)};
-            }
-            numberToIndex.put(nums[i], i);
+        if (s.length() < 2) {
+            return false;
         }
 
-        return null;
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (isCloseBracket(c)) {
+                if (stack.isEmpty() || !stack.peek().equals(getOpenBracket(c))) {
+                    return false;
+                }
+                stack.pop();
+                continue;
+            }
+            stack.push(c);
+        }
+
+        return stack.isEmpty();
+    }
+
+    private boolean isCloseBracket(char c) {
+        return c == ')' || c == '}' || c == ']';
+    }
+
+    private char getOpenBracket(char c) {
+        if (c == ')') {
+            return '(';
+        }
+
+        if (c == '}') {
+            return '{';
+        }
+
+        if (c == ']') {
+            return '[';
+        }
+
+        return ' ';
     }
 }
