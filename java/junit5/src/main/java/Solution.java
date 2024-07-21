62ff7abccaed00ef01bb683a771101a00a53282e
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-07-21 20:44:15 +0530

    Solution for Anagram

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -1,19 +1,33 @@
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Solution {
 
-    private TreeNode invertTree(TreeNode root) {
+    public boolean isAnagram(String s, String t) {
 
-        if (root == null) {
-            return null;
+        Map<Character, Integer> sOccurrences = s.chars().mapToObj(c -> (char) c)
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        c -> 1,
+                        Integer::sum,
+                        HashMap::new
+                ));
+
+        for (char charInT : t.toCharArray()) {
+            if (!sOccurrences.containsKey(charInT)) {
+                return false;
+            }
+            int numberOfOccurrences = sOccurrences.get(charInT);
+            if (numberOfOccurrences > 1) {
+                sOccurrences.put(charInT, --numberOfOccurrences);
+                continue;
+            }
+            sOccurrences.remove(charInT);
         }
 
-        TreeNode temp = root.left;
-
-        root.left = invertTree(root.right);
-        root.right = invertTree(temp);
-
-        return root;
+        return sOccurrences.isEmpty();
 
     }
+
 }
