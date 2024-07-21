e2db26b68e3102ff9ceb681408e2b12123eb8c48
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2024-07-21 19:58:00 +0530

    Solution for Invert Binary Tree

--- java/junit5/src/main/java/Solution.java
+++ java/junit5/src/main/java/Solution.java
@@ -2,35 +2,18 @@ import java.util.*;
 
 public class Solution {
 
-    private Set<Character> alphanumericChars = new HashSet<>(Arrays.asList(
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-            'u', 'v', 'w', 'x', 'y', 'z',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
+    private TreeNode invertTree(TreeNode root) {
 
-    public boolean isPalindrome(String s) {
-
-        List<Character> alphanumericS = new LinkedList<>();
-
-        for (char c : s.toCharArray()) {
-            c = Character.toLowerCase(c);
-            if (alphanumericChars.contains(c)) {
-                alphanumericS.add(c);
-            }
+        if (root == null) {
+            return null;
         }
 
-        int leftPtr = 0;
-        int rightPtr = alphanumericS.size() - 1;
+        TreeNode temp = root.left;
 
-        while (leftPtr < rightPtr) {
-            if (!alphanumericS.get(leftPtr).equals(alphanumericS.get(rightPtr))) {
-                return false;
-            }
-            leftPtr++;
-            rightPtr--;
-        }
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
 
-        return true;
+        return root;
 
     }
 }
